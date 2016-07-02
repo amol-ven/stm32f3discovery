f85e7f00cf64014b1c676991c4976657f71e707b
Amol Vengurlekar <amol.1991@gmail.com>
2016-07-02 17:18:18 -0600

    base. blinky working.
    Signed-off-by: Amol Vengurlekar <amol.1991@gmail.com>

---
+++ projects/blinky/blinky.d
@@ -0,0 +1,19 @@
+blinky.o: blinky.c \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/f3/memorymap.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/f3/rcc.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/f3/gpio.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ /home/amol/stm32f3discovery/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
